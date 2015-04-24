90219fc9e9fa7d3d098e63fca4d829b536d76a40
gravesr1993 <gravesr1993@gmail.com>
2015-04-24 13:04:05 -0600

    Added character
    Added character controls via integrating unity standard assets, created
    a state machine and ripped a sprite sheet, of a crab via the creative
    commons license on a game titled Glitch

---
+++ Project Crab/Assets/Scripts/PlayerHandler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHandler : MonoBehaviour {
+
+	private PlayerMover m_Character;
+	private bool m_Jump;
+
+	private void Awake()
+	{
+		m_Character = GetComponent<PlayerMover> ();
+	}
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+	void FixedUpdate()
+	{
+		// Read the inputs.
+		float h =Input.GetAxis ("Horizontal");
+		// Pass all parameters to the character control script.
+		m_Character.Move(h);
+	}
+}
---
+++ Project Crab/Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMover : MonoBehaviour {
+
+	private Rigidbody2D m_Rigidbody2D;
+	public float m_MaxSpeed;
+	// Use this for initialization
+	void Start () 
+	{
+		m_MaxSpeed = 10;
+		m_Rigidbody2D = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+	void FixedUpdate()
+	{
+	}
+	public void Move(float move)
+	{
+		m_Rigidbody2D.velocity = new Vector2(move*m_MaxSpeed, m_Rigidbody2D.velocity.y);
+	}
+}
